648640795ce2de074a0c8c8c757e799a29db553e
corazonnn <zero7364@icloud.com>
2021-07-19 00:50:17 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import PropsTypes from 'prop-types';
-function App() {
-  const profiles = [
-    {name: "ルフィ",age:19},
-    {name: "ゾロ",age:20},
-    {name: "ナミ",age:19},
-    {name: "サンジ",age:20},
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index)=>{
-          return <Onepiece name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  );
-}
-const Onepiece = (props) =>{
-  return <div>I am {props.name} and {props.age}!!!</div>
-}
-Onepiece.propsTypes = {
-  name: PropsTypes.string
+import React, { Component } from "react";
+import PropsTypes from "prop-types";
+import { connect } from "react-redux";
+import {increment,decrement} from '../actions'
+class App extends Component {
+
+  render() {
+    const props = this.props
+    return (
+      <React.Fragment>
+        <div>count: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
+      </React.Fragment>
+    );
+  }
 }
 
-export default App;
+//mapStateToPropsはstateの情報を取り出してcomponent内のpropsとしてマッピングする
+const mapStateToProps = state => ({value: state.count.value})
+
+//dispatch...あるactionが実行された際にreducerにtypeに応じた状態遷移を実行させるための関数
+const mapDispatchToProps = dispatch =>({
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+})
+
+//stateとactionを関連づける部分
+export default connect(mapStateToProps, mapDispatchToProps)(App)
+
+// export default App;
